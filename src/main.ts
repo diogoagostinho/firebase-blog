a8967ebfe8cf368678a764f724dbaf70153e5ba1
AGOSTINHO <LaaGostim.OFICIAL@gmail.com>
2024-01-08 12:36:29 +0000

    r0.3.0
    Added typescript, started hero slide mouseover envent

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
+const topSectionHero = document.getElementById("topSectionHero") as HTMLElement;
+const topSection = document.getElementById("topSection") as HTMLDivElement;
+const bottomSection = document.getElementById(
+  "bottomSection"
+) as HTMLDivElement;
 
+function heroSlide() {
+  let mouseOn = "top:0;";
+  let mouseOff = "top:-100vh;";
+
+  topSection.addEventListener("mouseenter", () => {
+    topSectionHero.setAttribute("style", mouseOn);
+  });
+
+  bottomSection.addEventListener("mouseenter", () => {
+    topSectionHero.setAttribute("style", mouseOff);
+  });
+}
+
+heroSlide();
